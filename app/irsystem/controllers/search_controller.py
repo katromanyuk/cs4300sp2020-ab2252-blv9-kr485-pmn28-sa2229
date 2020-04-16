839bb724f01744f49a1f7b35fee631565566dfe3
Shilpy <sa2229@cornell.edu>
2020-04-16 18:39:22 -0400

    Checking if movie querying works

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -5,18 +5,20 @@ from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
 project_name = "Clever Monkeys: Movie Recommendations Based on Music Preferences"
 net_id = "Amber Baez: ab2252, Betsy Vasquez Valerio: blv9, " \
-"Kateryna Romanyuk: kr485, Patrick Neafsey: pmn28, Shilpy Agarwal: sa2229"
+    "Kateryna Romanyuk: kr485, Patrick Neafsey: pmn28, Shilpy Agarwal: sa2229"
+
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	query = request.args.get('search1')
-	query2 = request.args.get('search2')
-	if not query or not query2:
-		output_message = ''
-		data = ['Please fill out both fields']
-	else:
-		output_message = "Your search: " + query + " and " + query2
-		music = find_music(query)
-		movie = find_movie(query2)
-		data= music + movie
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+    song = request.args.get('search1')
+    movie = request.args.get('search2')
+    artist = request.args.get('search3')
+    if not song or not movie:
+        output_message = ''
+        data = ['Please give us at least the song and movie!']
+    else:
+        output_message = "Your search: " + song + " and " + movie
+        music = find_music(song, artist)
+        movie = find_movie(movie)
+        data = music + movie
+    return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -2,35 +2,52 @@ import numpy as np
 import pandas as pd
 import lyricsgenius
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import requests
 
 TOKEN = 'sD0C3epnJdfOQQK4eIC45dHl-Qv7DipToGpuj1n4WeuG5_LDP1HKn31w5Cn1lOux'
 genius = lyricsgenius.Genius(TOKEN)
 genius.verbose = False
 genius.remove_section_headers = True
 
-def find_music(query):
+
+def find_music(song, artist):
     output = []
-    if ',' in query:
-        query = query.split(',')
-        result = genius.search_song(query[0], query[1])
+    if artist and song:
+        result = genius.search_song(song, artist)
         output.append('Song: '+result.title)
-        output.append('----------------')
         output.append('Artist: '+result.artist)
         output.append('----------------')
         output.append('Lyrics: '+result.lyrics)
         output.append('----------------')
-    else:
-        result = genius.search_artist(query, max_songs=5)
-        output.append('Artist: '+result.name)
+    elif song:
+        result = genius.search_song(song)
+        output.append('Song: '+result.name)
+        output.append('Artist: '+result.artist)
         output.append('----------------')
-        output.append('Top songs:')
+        output.append('Lyrics')
+        output.append(result.lyrics)
+        output.append('----------------')
+    elif artist:
+        result = genius.search_artist(artist, max_songs=5)
+        output.append('Artist: '+result.artist)
+        output.append('----------------')
+        output.append('Top 5 Songs for this artist:')
         i = 1
-        for song in result.songs:
-            output.append(str(i)+'. '+song.title)
-            i+=1
+        for x in result.songs:
+            output.append(str(i) + " " + x.title)
+            i += 1
         output.append('----------------')
     return output
 
-def find_movie(query):
-    output = ['Movie: ...']
+
+def find_movie(movie):
+    output = []
+    # find the movie plot summary using IMDB API
+    token = 'ce887dbd'
+    query = "http://www.omdbapi.com/?apikey=" + token + "&s=" + movie
+    params = {"r": "json"}
+    result = requests.request("GET", query)
+    plot = result[plot]
+    output.append(plot)
+
     return output
